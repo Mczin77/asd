9b2e07457cf74a12ecd4e7c1b8f0fb97930726d9
Mczin77 <mczin804@gmail.com>
2025-11-20 20:02:26 -0300

    Add validation handler for API requests

--- api/validate.js
+++ api/validate.js
@@ -1,1 +1,14 @@
+import fs from "fs";
 
+export default function handler(req, res) {
+  const { key } = req.query;
+
+  const db = JSON.parse(fs.readFileSync("db.json", "utf8"));
+  const found = db.keys.find(k => k.key === key);
+
+  if (!found) {
+    return res.json({ valid: false });
+  }
+
+  return res.json({ valid: true });
+}
